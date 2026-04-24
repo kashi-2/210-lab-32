5af8ebe57668e79eae859d7aed8bdbb853af1c85
Akashdeep <akashdeep.thind.singh@gmail.com>
2026-04-23 22:02:44 -0700

    milestone 2

--- main.cpp
+++ main.cpp
@@ -29,3 +29,40 @@ int main() {
     displayQueue(lane);
 
     int timeStep = 1;
+
+    //Milestone 2 
+    while (!lane.empty()) {
+        int r =raand() % 100;
+
+        cout << "Time: " << timeStep << " "; 
+
+        if (r < 55) {
+            
+            Car frontCar = lane.front();
+            lane.pop_front();
+
+            cout << "Operation: Car paid: ";
+            frontCar.print();
+            cout << endl;
+        }
+        else {
+            Car newCar;
+            lane.push_back(newCar);
+
+            cout << "Operation: Joined lane: ";
+            newCar.print();
+            cout << endl;
+        }
+
+        cout << "Queue: \n";
+        if (lane.empty()) {
+            cout << " Empty\n";
+        }
+        else {
+            displayQueue(lane);
+        }
+
+        timeStep++; 
+    }
+    return 0;
+}
